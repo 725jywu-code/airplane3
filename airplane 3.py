63719119906214a6023538baac070da3ba1e4b67
725jywu-code <725jywu@gmail.com>
2026-01-11 13:42:36 +0800

    顏色與視覺設定
    讓不同狀態有明確顏色區分

--- airplane 3.py
+++ airplane 3.py
@@ -5,3 +5,9 @@ import random                            # 產生隨機飛機形狀與位置
 GRID_SIZE = 10           # 棋盤大小為 10x10
 CELL_SIZE = 40           # 主棋盤格子大小
 PREVIEW_CELL_SIZE = 20   # 右側預覽飛機格子大小
+
+COLOR_DEFAULT = "#DDDDDD"   # 未翻開格子
+COLOR_MISS = "white"        # 點到空格
+COLOR_BODY = "#5555FF"      # 飛機機身
+COLOR_HEAD = "#FF4444"      # 飛機機頭
+COLOR_TEXT = "black"
